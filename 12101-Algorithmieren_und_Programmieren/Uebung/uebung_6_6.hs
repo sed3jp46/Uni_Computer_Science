5e2e5ed29d4f7907701cc7fc1c5909555f5d8ccc
Jean-Pierre Kasperschinski <Jean-Pierre.Kasperschinski@b-tu.de>
2024-06-08 17:14:28 +0200

    up

--- 12101-Algorithmieren_und_Programmieren/Uebung/uebung_6_6.hs
+++ 12101-Algorithmieren_und_Programmieren/Uebung/uebung_6_6.hs
@@ -1,23 +1,28 @@
+module Menge (Menge(), leer, einfuegen, loeschen, vereinigung, schnitt, ) where
 import Data.List (sort)
-module Menge () where
 
- data Menge el = Menge [el] deriving Eq
- 
- instance (Show el) => Show (Menge el) where
+
+data Menge el = Menge [el] deriving Eq
+
+instance (Show el) => Show (Menge el) where
      show (Menge liste) = "{" ++ init (tail (show liste)) ++ "}"
- 
- leer :: Menge el
- leer = Menge []
- 
- einfuegen :: (Ord el) => el -> Menge el -> Menge el
- einfuegen el (Menge []) = Menge [el]
- einfuegen el (Menge liste) = (Menge (liste ++ [el]))
 
- loeschen :: (Ord el) => el -> Menge el -> Menge el
- loeschen b (Menge el) = (Menge [ a | a <- (el) , a /= b ])
 
- vereinigung :: (Ord el) => Menge el -> Menge el -> Menge el
- vereinigung (Menge el) (Menge al) = (Menge ((el) ++ (al)))
+leer :: Menge el
+leer = Menge []
 
- schnitt :: (Ord el) => Menge el -> Menge e1 -> Menge e1
- schnitt (Menge al) (Menge el) = 
+einfuegen :: (Ord el) => el -> Menge el -> Menge el
+einfuegen el (Menge []) = Menge [el]
+einfuegen el (Menge liste) = Menge (liste ++ [el])
+
+loeschen :: (Ord el) => el -> Menge el -> Menge el
+loeschen b (Menge el) = Menge [ a | a <- el , a /= b ]
+
+vereinigung :: (Ord el) => Menge el -> Menge el -> Menge el
+vereinigung (Menge el) (Menge al) = Menge (el ++ al)
+
+schnitt :: (Ord el) => Menge el -> Menge el -> Menge el
+schnitt (Menge al) (Menge el) =
+    if length [al] < length [el] then Menge ([ x | x <- al, x `elem` el])
+    else Menge ([ x | x <- el, x `elem` al])
+
