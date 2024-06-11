ee4621b6bc19d3d04e1acceb8fc52d154b109710
Jean-Pierre Kasperschinski <Jean-Pierre.Kasperschinski@b-tu.de>
2024-06-11 16:54:29 +0200

    CNN

--- 12101-Algorithmieren_und_Programmieren/AB_8/Hof.hs
+++ 12101-Algorithmieren_und_Programmieren/AB_8/Hof.hs
@@ -23,7 +23,45 @@ takeWhile _ [] = []
 takeWhile f (a:as) = [a | a <- a:as, f a]
 
 filter :: (a -> Bool) -> [a] -> [a]
-filter f (a:as) = as
+filter f [] = []
+filter f [a] = [a]
+filter f (a:as) =
+    if f a then a : filter f as
+    else filter f as
 
 partition :: (a -> Bool) -> [a] -> ([a], [a])
 partition f (a:as) = ([a],as)
+
+zipwith :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipwith f [] bs = []
+zipwith f as [] = []
+zipwith f (a:as) (b:bs) = f a b : zipwith f as bs
+
+(.) :: (b -> c) -> (a -> b) -> (a -> c)
+f . g = \x -> f (g x)
+
+uncurry :: (a -> b -> c) -> (a,b) -> c
+uncurry f (x,y) = f x y
+
+curry :: ((a,b) -> c ) -> a -> b -> c
+curry f x y = f (x,y)
+
+foldl :: (acc -> el -> acc) -> acc -> [el] -> acc
+foldl f z [] = z
+foldl f z (x:xs) = foldl f (f z x) xs
+
+foldr :: (el -> acc -> acc) -> acc -> [el] -> acc
+foldr f z [] = z
+foldr f z (x:xs) = f x (foldr f z xs)
+
+foldr1 :: (a -> a -> a) -> [a] -> a
+foldr1 f [x] = x
+foldr1 f (x:xs) = f x (foldr1 f xs)
+
+foldl1 :: (a -> a -> a) -> [a] -> a
+foldl1 f [x] = x
+foldl1 f (x:xs) = foldl f x xs
+
+--alleAnwenden :: [arg -> wert] -> arg -> [wert]
+
+--hintereinanderAnwenden :: [wert -> wert] -> wert -> wert
