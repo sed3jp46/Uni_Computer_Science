350c9732f409b547f003b36006e051a8dddc5433
Jean-Pierre Kasperschinski <Jean-Pierre.Kasperschinski@b-tu.de>
2024-06-11 15:06:51 +0200

    vl

--- 12101-Algorithmieren_und_Programmieren/Vorlesung/VL_11-06.hs
+++ 12101-Algorithmieren_und_Programmieren/Vorlesung/VL_11-06.hs
@@ -1,4 +1,8 @@
-reverse :: [a]->[a]
-reverse [] = []
-reverse (x:xs) = reverse xs ++ [x]
-
+data Tree a = Leaf
+    | Node a (Tree a) (Tree a)
+    
+    insertTree :: Ord a => a -> Tree a -> Tree a
+    insertTree a Leaf = Node a Leaf Leaf
+    insertTree a t@(Node n l r) = if (a==n) then t
+                                      else if (a < n) then (Node n (insertTree a l) r)
+                                          else (Node n l (insertTree a r)) 
