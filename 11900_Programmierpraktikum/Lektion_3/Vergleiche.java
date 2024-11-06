6e7cd20964b9ff3568d9bd0e922481de8ac7bd72
Jean-Pierre Kasperschinski <Jean-Pierre.Kasperschinski@b-tu.de>
2024-11-06 12:20:35 +0100

    help i got chased by killer klowns

---
+++ 11900_Programmierpraktikum/Lektion_3/Vergleiche.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+import javax.swing.SpinnerNumberModel;
+
+public class Vergleiche {
+    
+    private static Scanner s = new Scanner(System.in);
+    
+    public static void main(String [] args) {
+        
+        
+        
+    }
+    
+    public static void compareNames() {
+       String name1;
+       String name2;
+       
+       System.out.println("Gebe ersten Namen an");
+       name1 = s.next();
+
+       System.out.println("Gebe zweiten Namen an");
+       name2 = s.next();
+
+       int name1_lgth = name1.length();
+       int name2_lgth = name2.length();
+
+       if (name1_lgth > name2_lgth) {
+        System.out.println(name1 + " ist länger als " + name2);
+       } else if (name2_lgth > name1_lgth) {
+        System.out.println(name2 + " ist länger als " + name1);
+       } else {
+        System.out.println(name1 + " ist genauso lang wie "+ name2);
+       }
+        
+    }
+    
+    public static void compareNumbers() {
+        int zahl1;
+        int zahl2;
+        int zahl3;
+
+        System.out.println("Gebe die erste Zahl an");
+        zahl1 = s.nextInt();
+
+        System.out.println("Gebe die zweite Zahl an");
+        zahl2 = s.nextInt();
+
+        System.out.println("Gebe die dritte Zahl an");
+        zahl3 = s.nextInt();
+
+        int Summe = zahl1+zahl2+zahl3 ;
+        int durchschnitt = Summe/3;
+
+        System.out.println("Die Summe der 3 Zahlen ist: "+ Summe + " und der Durchschnitt beträgt: "+ durchschnitt);
+        
+        
+    }
+}
