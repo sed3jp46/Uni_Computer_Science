fffd568ddc2124109a36b79d3e7c3aeca9f27276
Jean-Pierre.Kasperschinski <Jean-Pierre.Kasperschinski@b-tu.de>
2024-11-19 18:16:52 +0100

    java

--- 11900_Programmierpraktikum/Lektion_3/Scooter.java
+++ 11900_Programmierpraktikum/Lektion_3/Scooter.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-java.util.LinkedList;
 
 public class Scooter {
     
@@ -10,10 +9,15 @@ public class Scooter {
 		int c; //choice
 		int m; //meter
 		double price = 5;
-		
+		String vendor = "Nby";
+
 		System.out.println("Wie viele Meter sollen gefahren werden ?");
 		m = s.nextInt();
 
+		double price1 = 1+((m/500)*0.5);
+		double price2 = ((m/500)*0.75);
+		double price3 = 5;
+		double price4 = 1+((m/100)*0.11);
 		System.out.println("Welchen Anbieter willst du nutzen ?");
 		System.out.println("1 = Anbieter 1: 1€ Unlockgebühr, 0.50€/500m");
 		System.out.println("2 = Anbieter 2: 0€ Unlockgebühr, 0.75€/500m");
@@ -23,28 +27,42 @@ public class Scooter {
 		c = s.nextInt();
 		switch(c){
 			case 1:
-				price = 1+((m/500)*0.5);
+				price = price1;
 				break;
 			case 2:
-				price = ((m/500)*0.75);
+				price = price2;
 				break;
 			case 3:
-				price = 5;
+				price = price3;
 				break;
 			case 4:
-				price = 1+((m/100)*0.11);
+				price = price4;
 				break;
 			case 5:
-				price = 
+				if (price1 < price2  && price1 < 5 && price < price4) {
+					price = price1;
+					vendor = "Anbieter 1";
+				}
+				else if (price2 < price1  && price2 < 5 && price2 < price2) {
+					price = price2;
+					vendor = "Anbieter 2";
+				}
+				else if (price2 < price1  && price2 < 5 && price2 < price2) {
+					vendor = "Anbieter 3";
+				}
+				else {
+					price = price4;
+					vendor = "Anbieter 4";
+				}
 			default :
 				System.err.print("Wählen sie eine Zahl zwischen 1-5 bitte");
 				System.exit(0);
 		}
         
         /////System.out.print( km + " Kilometer sollen gefahren werden.");
-        //System.out.print(" Der Scooter wurde entriegelt.");
+        System.out.print(" Der Scooter wurde entriegelt.");
         System.out.print(" Sie bezahlen " + (float)price + " €.");
-        //System.out.println(" Vielen Dank, dass Sie sich fuer Anbieter " + vendor + " entschieden haben!");
+        System.out.println(" Vielen Dank, dass Sie sich fuer Anbieter " + vendor + " entschieden haben!");
         
 		s.close();
 	}
