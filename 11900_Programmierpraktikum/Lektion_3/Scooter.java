4adc282f9db646c36956938dd42dbc0b221114e2
Jean-Pierre.Kasperschinski <Jean-Pierre.Kasperschinski@b-tu.de>
2024-11-19 12:45:43 +0100

    scooter.java

--- 11900_Programmierpraktikum/Lektion_3/Scooter.java
+++ 11900_Programmierpraktikum/Lektion_3/Scooter.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+java.util.LinkedList;
 
 public class Scooter {
     
@@ -6,20 +7,46 @@ public class Scooter {
 
 	public static void main(String[] args) {
 		
+		int c; //choice
+		int m; //meter
+		double price = 5;
 		
-		int m;
-		float km;
 		System.out.println("Wie viele Meter sollen gefahren werden ?");
 		m = s.nextInt();
-		
-		km = (float)m/1000;
+
+		System.out.println("Welchen Anbieter willst du nutzen ?");
+		System.out.println("1 = Anbieter 1: 1€ Unlockgebühr, 0.50€/500m");
+		System.out.println("2 = Anbieter 2: 0€ Unlockgebühr, 0.75€/500m");
+		System.out.println("3 = Anbieter 3: 5€ Unlockgebühr, 0.00€/500m");
+		System.out.println("4 = Anbieter 4: 1€ Unlockgebühr, 0.11€/100m");
+		System.out.println("5 = Komfortfunktion: sucht ihnen gleich den besten Anbieter");
+		c = s.nextInt();
+		switch(c){
+			case 1:
+				price = 1+((m/500)*0.5);
+				break;
+			case 2:
+				price = ((m/500)*0.75);
+				break;
+			case 3:
+				price = 5;
+				break;
+			case 4:
+				price = 1+((m/100)*0.11);
+				break;
+			case 5:
+				price = 
+			default :
+				System.err.print("Wählen sie eine Zahl zwischen 1-5 bitte");
+				System.exit(0);
+		}
         
-        System.out.print( km + " Kilometer sollen gefahren werden.");
-        System.out.print(" Der Scooter wurde entriegelt.");
-        s.close();
-        //System.out.print(" Sie bezahlen " + String.format("%.2f", price) + "€.");
+        /////System.out.print( km + " Kilometer sollen gefahren werden.");
+        //System.out.print(" Der Scooter wurde entriegelt.");
+        System.out.print(" Sie bezahlen " + (float)price + " €.");
         //System.out.println(" Vielen Dank, dass Sie sich fuer Anbieter " + vendor + " entschieden haben!");
         
+		s.close();
 	}
 
 
